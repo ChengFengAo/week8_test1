884172b3082b1dc498e73f88f973fffead4e7a00
ChengFeng AO <414356022@qq.com>
2018-05-04 22:16:10 +0800

    the function findByGenderEquals()

--- src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/EmployeeRepository.java
@@ -3,7 +3,10 @@ package com.example.employee.restfulapi.repository;
 import com.example.employee.restfulapi.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+   List<Employee> findByGenderEquals(String male);
+
 }
