fd8c27042fa18042d08e0a89f5de4df1c72d18c2
ChengFeng AO <414356022@qq.com>
2018-05-04 17:11:35 +0800

    add functions to CompanyRepository

--- src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
+++ src/main/java/com/example/employee/restfulapi/repository/CompanyRepository.java
@@ -1,9 +1,15 @@
 package com.example.employee.restfulapi.repository;
 
 import com.example.employee.restfulapi.entity.Company;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
+    Company findById(int id);
+
+    @Override
+    Page<Company> findAll(Pageable pageable);
 }
